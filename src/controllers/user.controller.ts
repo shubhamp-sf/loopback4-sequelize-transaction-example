5f5eeca6db2e7404d40e42982a9bbd04e7666db3
Shubham P <shubham.prajapat@sourcefuse.com>
2023-03-03 13:10:04 +0530

    add artifacts and loopback transaction example in user controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,191 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  IsolationLevel,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  del,
+  get,
+  getModelSchemaRef,
+  param,
+  patch,
+  post,
+  put,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import {User} from '../models';
+import {UserRepository} from '../repositories';
+
+export class UserController {
+  constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+  ) {}
+
+  @get('/transaction-example')
+  @response(200, {
+    description: 'User model instance',
+    content: {'application/json': {schema: getModelSchemaRef(User)}},
+  })
+  async transactionExample(): Promise<User> {
+    // Begin a new transaction.
+    // It's also possible to call `userRepo.dataSource.beginTransaction` instead.
+    const tx = await this.userRepository.beginTransaction({
+      isolationLevel: IsolationLevel.SERIALIZABLE,
+    });
+
+    try {
+      // Then, we do some calls passing this transaction as an option:
+      const user = await this.userRepository.create(
+        {
+          firstName: 'Jon',
+          lastName: 'Doe',
+        },
+        {transaction: tx},
+      );
+
+      await this.userRepository.updateById(
+        user.id,
+        {
+          firstName: 'John',
+        },
+        {transaction: tx},
+      );
+
+      // If the execution reaches this line, no errors were thrown.
+      // We commit the transaction.
+      await tx.commit();
+
+      // user after commit
+      return this.userRepository.findById(user.id);
+    } catch (error) {
+      // If the execution reaches this line, an error was thrown.
+      // We rollback the transaction.
+      await tx.rollback();
+      throw error;
+    }
+  }
+
+  @post('/users')
+  @response(200, {
+    description: 'User model instance',
+    content: {'application/json': {schema: getModelSchemaRef(User)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {
+            title: 'NewUser',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    user: Omit<User, 'id'>,
+  ): Promise<User> {
+    return this.userRepository.create(user);
+  }
+
+  @get('/users/count')
+  @response(200, {
+    description: 'User model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(@param.where(User) where?: Where<User>): Promise<Count> {
+    return this.userRepository.count(where);
+  }
+
+  @get('/users')
+  @response(200, {
+    description: 'Array of User model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(User, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(@param.filter(User) filter?: Filter<User>): Promise<User[]> {
+    return this.userRepository.find(filter);
+  }
+
+  @patch('/users')
+  @response(200, {
+    description: 'User PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {partial: true}),
+        },
+      },
+    })
+    user: User,
+    @param.where(User) where?: Where<User>,
+  ): Promise<Count> {
+    return this.userRepository.updateAll(user, where);
+  }
+
+  @get('/users/{id}')
+  @response(200, {
+    description: 'User model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(User, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.number('id') id: number,
+    @param.filter(User, {exclude: 'where'}) filter?: FilterExcludingWhere<User>,
+  ): Promise<User> {
+    return this.userRepository.findById(id, filter);
+  }
+
+  @patch('/users/{id}')
+  @response(204, {
+    description: 'User PATCH success',
+  })
+  async updateById(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {partial: true}),
+        },
+      },
+    })
+    user: User,
+  ): Promise<void> {
+    await this.userRepository.updateById(id, user);
+  }
+
+  @put('/users/{id}')
+  @response(204, {
+    description: 'User PUT success',
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() user: User,
+  ): Promise<void> {
+    await this.userRepository.replaceById(id, user);
+  }
+
+  @del('/users/{id}')
+  @response(204, {
+    description: 'User DELETE success',
+  })
+  async deleteById(@param.path.number('id') id: number): Promise<void> {
+    await this.userRepository.deleteById(id);
+  }
+}
---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,14 @@
+import {inject} from '@loopback/core';
+import {DefaultTransactionalRepository} from '@loopback/repository';
+import {DbDataSource} from '../datasources';
+import {User, UserRelations} from '../models';
+
+export class UserRepository extends DefaultTransactionalRepository<
+  User,
+  typeof User.prototype.id,
+  UserRelations
+> {
+  constructor(@inject('datasources.db') dataSource: DbDataSource) {
+    super(User, dataSource);
+  }
+}
